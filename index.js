d08751c3967b465b705588ffa5877c037fb62e86
Rijad Burović <rburovic1@etf.unsa.ba>
2024-03-08 10:45:58 +0100

    connected frontend and backend

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/html"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/html/chat.html");
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
