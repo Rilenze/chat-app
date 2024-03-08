d93a0b7b6075df67c42429e938c7fced3692ab63
Rijad Burović <rburovic1@etf.unsa.ba>
2024-03-08 10:56:33 +0100

    socket.io connection implemented

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/public/html"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/html/chat.html");
+});
+
+// Running when someone connects
+io.on("connection", (socket) => {
+  console.log("New web socket connection");
 });
 
 server.listen(3000, () => console.log("Server running on port 3000"));
