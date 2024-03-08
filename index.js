f9979dec26cffe8837eb156d012aa832d46d6ca2
Rijad Burović <rburovic1@etf.unsa.ba>
2024-03-08 11:43:47 +0100

    Emitting messages when user connects or diconnects

--- index.js
+++ index.js
@@ -13,9 +13,19 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/html/chat.html");
 });
 
-// Running when someone connects
+// Running when client connects
 io.on("connection", (socket) => {
   console.log("New web socket connection");
+
+  socket.emit("message", "Message sent!");
+
+  // runs when user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // runs when user disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 server.listen(3000, () => console.log("Server running on port 3000"));
